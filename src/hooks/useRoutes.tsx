d85df04b446b09b9fddbc8a220d1750884fbc95f
Lady_Proger <facktartar@mail.com>
2021-01-28 20:09:32 +0300

    add clean useRoutes

---
+++ src/hooks/useRoutes.tsx
@@ -0,0 +1,30 @@
+import {useEffect, Suspense} from "react";
+import {useObserver} from "mobx-react-lite";
+import { Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+import {useHttp} from "./useHttp";
+
+export const useRoutes = () => {
+    const {loading, request} = useHttp()
+
+    useEffect(() => {
+
+    },[request])
+
+    const isAuth = false
+    return useObserver(() => (
+        <Switch>
+            <Suspense fallback={<div>Some load...</div>}>
+                {loading? <>
+
+                </>:<>
+                    {isAuth? <>
+
+                    </>:<>
+
+                    </>}
+                </>}
+            </Suspense>
+        </Switch>
+    ))
+}
