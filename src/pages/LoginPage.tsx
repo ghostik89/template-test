94a6db347343525d361963f20ec0827b92a78a63
Lady_Proger <facktartar@mail.com>
2021-03-24 15:34:54 +0300

    remove unused links

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,8 +5,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -46,7 +44,7 @@ type FormData = {
 export default function LoginPage() {
     const classes = useStyles();
     const { register, handleSubmit } = useForm<FormData>();
-    const [_, setCookie] = useCookies(['user']);
+    const [, setCookie] = useCookies(['user']);
     const user = useUser()
 
     const onSubmit = (data: any) =>{
@@ -103,18 +101,6 @@ export default function LoginPage() {
                     >
                         Sign In
                     </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
                 </form>
             </div>
             <Copyright/>
